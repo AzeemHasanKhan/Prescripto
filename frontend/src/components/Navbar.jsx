dce8651bbc4420713d0bfffd32c03088f5673ec3
Azeem Hasan Khan <khanazeemhasan@gmail.com>
2024-10-22 17:13:16 +0530

    navbar changes 2nd commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,12 +6,12 @@ import { AppContext } from "../context/AppContext";
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  const { token, setToken,userData } = useContext(AppContext);
+  const { token, setToken, userData } = useContext(AppContext);
 
-  const logout = () =>{
+  const logout = () => {
     setToken(false);
-    localStorage.removeItem("token")
-  }
+    localStorage.removeItem("token");
+  };
 
   const navigate = useNavigate();
 
@@ -40,15 +40,18 @@ const Navbar = () => {
           <li className=" py-1">Contact</li>
           <hr className=" border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
+        <NavLink
+          to="https://prescripto-admin-0zvh.onrender.com/"
+          target="_blank"
+        >
+          <li className=" py-1 px-3 border bg-gray-300">Admin</li>
+          <hr className=" border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
       </ul>
-      <div className=" flex items-center gap-4">
+      <div className=" flex items-center gap-4 rounded-xl">
         {token && userData ? (
           <div className=" flex items-center gap-2 cursor-pointer group relative">
-            <img
-              className=" w-8 rounded-full"
-              src={userData.image}
-              alt=""
-            />
+            <img className=" w-8 rounded-full" src={userData.image} alt="" />
             <img className=" w-2.5" src={assets.dropdown_icon} alt="" />
             <div className=" absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className=" min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
@@ -114,6 +117,12 @@ const Navbar = () => {
             </NavLink>
             <NavLink onClick={() => setShowMenu(false)} to="/contact">
               <p className=" px-4 py-2 rounded inline-block">Contact</p>
+            </NavLink>
+            <NavLink
+              to="https://prescripto-admin-0zvh.onrender.com/"
+              target="_blank"
+            >
+              <p className=" px-4 py-2 rounded inline-block">Admin</p>
             </NavLink>
           </ul>
         </div>
